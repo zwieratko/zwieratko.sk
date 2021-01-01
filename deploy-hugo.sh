eec0c082aaa797abe0d4c04a0b760e713d0d8f14
zwieratkoDellXPS <zwieratko1@gmail.com>
2021-01-01 08:28:01 +0100

    oprava: PWA-4 sw.js generované číslo verzie
    Problem:
    Pokiaľ vykonám zmenu obsahu (téma, článok, ...) ale zároveň sa nezmení
    sw.js tak sa neaktualizujú súbory v cache na novú upravenú verziu
    a teda pri mojej stratégii kešovania - najskôr cache - sa zmeny
    neprejavia na webe, lebo stále bude používať stará verzie súborov z
    cache
    Solution:
    - postačuje aby na sw.js bol zmenený čo i len jeden byte a do cache sa
    nahrajú všetky súbory na novo
    - pri každom spustení skriptu deploy-hugo.sh dočasne vygenerujem náhodné
    číslo a to pomocou sed vložím do prvého riadku sw.js ako konštantu
    version
    - a teda spôsobím zmenu v súbore sw.js aj keď tam nepotrebujem nič meniť
    - takto dočasne zmenený sw.js sa odošle na produkčný server
    - na záver sa opäť pomocou sed zmení hodnota konštanty version na
    pôvodnú hodnotu 1, aby to git nebral ako zmenu
    Note: NA

--- deploy-hugo.sh
+++ deploy-hugo.sh
@@ -9,6 +9,10 @@ USER=uid108571
 HOST=shellserver-3.websupport.sk
 DIR=zwieratko.sk/web/   # the directory where your web site files should go
 
+RANDOMVERSIONNUMBER=$(shuf -i 2000-65000 -n 1)
+
+sed -i "s/const\sversion\s=\s.*;/const version = '$RANDOMVERSIONNUMBER';/g" ./static/sw.js
+
 hugo --gc --minify --cleanDestinationDir && \
 echo 'Brotli compression started ...' && \
 for x in `/usr/bin/find $HOME/web/zwieratko.sk/public -type f -name '*.html' -o -name '*.css' -o -name '*.js'`; do
@@ -16,5 +20,7 @@ for x in `/usr/bin/find $HOME/web/zwieratko.sk/public -type f -name '*.html' -o 
 done && \
 echo 'Brotli compression is done.' && \
 rsync -avzh -e "ssh -p 17036" --delete public/ ${USER}@${HOST}:~/${DIR}
+
+sed -i "s/const\sversion\s=\s.*;/const version = '1';/g" ./static/sw.js
 
 exit 0
